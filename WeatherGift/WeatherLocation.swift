6564bef57b5dfb9601c91ecf2640b1800c859b70
Connor Goodman <goodmaco@bc.edu>
2021-10-11 16:31:49 -0400

    with .gitignore

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Connor Goodman on 10/10/21.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
+
