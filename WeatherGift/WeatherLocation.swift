c3bdef6963d13169848e7b8adf12f7ba39176726
Connor Goodman <goodmaco@bc.edu>
2021-10-12 15:27:43 -0400

    Completed Week 7 - WeatherGift (6.1-6.11)

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -7,9 +7,46 @@
 
 import Foundation
 
-struct WeatherLocation{
+class WeatherLocation: Codable{
     var name: String
     var latitude: Double
     var longitude: Double
+    
+    init(name: String, latitude: Double, longitude: Double){
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    func getData() {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
+        
+        print("we are accessing the url \(urlString)")
+        
+        // Create a URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR could not create a url from \(urlString)")
+            return
+        }
+        
+        // Creata a URL session
+        let session = URLSession.shared
+        
+        // Get data with .dataTask method
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            // Deal with the data
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("\(json)")
+            } catch {
+                print("JSON ERROR: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
 }
-
